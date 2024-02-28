7ccca8a3cf89a47c4b831f020d9aceaa83623b21
kazukihori <git123.hoppy@gmail.com>
2024-02-28 17:44:26 +0900

    課題修正

--- lesson_ruby/janken.rb
+++ lesson_ruby/janken.rb
@@ -31,21 +31,25 @@ class Attimuite_game
             puts "相手：#{janken_tool[computer_hand]}を出しました"
             puts "あいこです"
 
-            return  true 
+            return  true
             
             elsif (my_hand == 0 && computer_hand == 1) || (my_hand == 1 && computer_hand == 2) || (my_hand == 2 && computer_hand == 0)
                 puts "あなた：#{janken_tool[my_hand]}を出しました"
                 puts "相手：#{janken_tool[computer_hand]}を出しました"
                 puts "じゃんけんはあなたの勝ちです"
 
-                return attimuite_hoi_myself
+                @winner = "あなた"
+
+                return attimuite_hoi
 
             elsif (my_hand == 0 && computer_hand == 2) || (my_hand == 1 && computer_hand == 0) || (my_hand == 2 && computer_hand == 1)
                 puts "あなた：#{janken_tool[my_hand]}を出しました"
                 puts "相手：#{janken_tool[computer_hand]}を出しました"
                 puts "じゃんけんはコンピュータの勝ちです"
 
-                return attimuite_hoi_computer
+                @winner = "コンピュータ"
+
+                return attimuite_hoi
             end
         end
 
@@ -55,7 +59,7 @@ class Attimuite_game
         
     end
 
-    def attimuite_hoi_myself
+    def attimuite_hoi
         puts "あっち向いて〜"
         puts "0(上)1(下)2(左)3(右)"
 
@@ -70,50 +74,36 @@ class Attimuite_game
         attimuite_my_hand = @user_attimuite_command
         attimuite_computer_hand = rand(0..3)
 
-        if attimuite_my_hand == attimuite_computer_hand
-            puts "あなた：#{attimuite_hoi_tool[attimuite_my_hand]}"
-            puts "相手 : #{attimuite_hoi_tool[attimuite_computer_hand]}"
-            puts "あなたの完全勝利です"
-            
-            exit
-        else 
-            puts "あなた：#{attimuite_hoi_tool[attimuite_my_hand]}"
-            puts "相手 : #{attimuite_hoi_tool[attimuite_computer_hand]}"
-            puts "勝負つかず、、もう一度じゃんけんからやり直し"
+        if @user_attimuite_command > 3 
+            puts  "不正な値です。もう一度入力してください"
+            return false
+        else
+            if attimuite_my_hand == attimuite_computer_hand && @winner == "あなた"
+                puts "あなた：#{attimuite_hoi_tool[attimuite_my_hand]}"
+                puts "相手 : #{attimuite_hoi_tool[attimuite_computer_hand]}"
+                puts "あなたの完全勝利です"
+                
+                exit
+            elsif attimuite_my_hand == attimuite_computer_hand && @winner == "コンピュータ"
+                puts "相手:#{attimuite_hoi_tool[attimuite_computer_hand]}"
+                puts "あなた: #{attimuite_hoi_tool[attimuite_my_hand]}"
+                puts "コンピュータの完全勝利です"
 
-            return janken
+                exit
+            else
+                puts "相手：#{attimuite_hoi_tool[attimuite_computer_hand]}"
+                puts "あなた : #{attimuite_hoi_tool[attimuite_my_hand]}"
+                puts "勝負つかず、、もう一度じゃんけんからやり直し"
+
+                return janken
+            end
         end
+
+                
+        
     end
 
-    def attimuite_hoi_computer
-        puts "あっち向いて〜"
-        puts "0(上)1(下)2(左)3(右)"
-
-        @user_attimuite_command = gets.chomp.to_i
-
-        puts "ホイ！"
-        puts "----------------------"
-
-
-        attimuite_hoi_tool = ["上","下","左","右"]
-
-        attimuite_my_hand = @user_attimuite_command
-        attimuite_computer_hand = rand(0..3)
-
-        if attimuite_my_hand == attimuite_computer_hand
-            puts "相手:#{attimuite_hoi_tool[attimuite_computer_hand]}"
-            puts "あなた: #{attimuite_hoi_tool[attimuite_my_hand]}"
-            puts "コンピュータの完全勝利です"
-            
-            exit
-        else 
-            puts "相手：#{attimuite_hoi_tool[attimuite_computer_hand]}"
-            puts "あなた : #{attimuite_hoi_tool[attimuite_my_hand]}"
-            puts "勝負つかず、、もう一度じゃんけんからやり直し"
-
-            return janken
-        end
-    end
+    
 
 
     
@@ -124,18 +114,21 @@ end
 battle1 = Attimuite_game.new()
 next_game = battle1.janken()
 
-
-while next_game do
-    next_game = battle1.janken()
+if next_game == true
+    while next_game do
+    battle1.janken
+    end
+else next_game == false
+    until next_game do
+    battle1.attimuite_hoi
+    end
 end
+
+
+
     
 
-if next_game == attimuite_hoi_myself
-    battle1.attimuite_hoi_myself
-else next_game == attimuite_hoi_computer
-    battle1.attimuite_hoi_computer
-end
- 
+
 
 
 
